180e212d2f0ee113a4be4193368ff883c2f8a1a5
cricrankers <cricrankers@gmail.com>
2024-10-20 20:38:53 +0530

    deployement changes

--- src/index.js
+++ src/index.js
@@ -6,8 +6,7 @@ const Router  = require('./routes/routes.index');
 const path = require('path');
 const {connectDB} = require('./db/dbConnect');
 
-//database connection
-connectDB();
+
 
 //routes
 app.use("/",Router);
@@ -19,5 +18,9 @@ app.set("views",path.resolve(__dirname,'views/pages'));
 //serving static files
 app.use(express.static(path.resolve(__dirname,'../public')))
 
-app.listen(PORT,()=>{console.log(`Server started at port ${PORT}`)});
+//database connection
+connectDB().then(()=>{
+    app.listen(PORT,()=>{console.log(`Server started at port ${PORT}`)});
+
+})
 
