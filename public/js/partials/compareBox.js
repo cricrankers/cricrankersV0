bd380b10bd64378f3f9e1270c6999805f3019b6a
cricrankers <cricrankers@gmail.com>
2024-10-23 23:20:09 +0530

    fixed drawer

---
+++ public/js/partials/compareBox.js
@@ -0,0 +1,11 @@
+const compareButtons = document.querySelectorAll('.compare-container');
+
+compareButtons.forEach(button => {
+    button.addEventListener('click', () => {
+       initCompareBox();
+    });
+});
+
+function initCompareBox(){
+    
+}
